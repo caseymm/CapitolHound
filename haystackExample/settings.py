cad1b08082c0fef3fcb3cb92fa25870434634fe3
caseymm <caseymm@gmail.cpom>
2013-10-28 12:39:54 -0400

    settings options

--- haystackExample/settings.py
+++ haystackExample/settings.py
@@ -21,6 +21,8 @@ DATABASES = {
   
 }
 
+OPTIONS = {'autocommit': True,}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
